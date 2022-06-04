e870dce3c13a9267b62f7778e060eb212335d879
Lauri <laurianarodrigueslima@gmail.com>
2022-06-04 20:53:28 -0300

    faq component

---
+++ index.js
@@ -0,0 +1,10 @@
+let answers = document.querySelectorAll(".boxQuestions");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
